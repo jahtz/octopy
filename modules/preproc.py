f79774f0595624c13e93bddb989f145090cf9f40
jatzelberger <jatzelberger@gmail.com>
2024-04-30 22:37:43 +0200

    add preprocessing

--- modules/preproc.py
+++ modules/preproc.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 
 import numpy as np
-from kraken.binarization import nlbin
 from PIL import Image
+from kraken.binarization import nlbin
+from kraken.blla import is_bitonal
 from scipy.ndimage import percentile_filter, zoom, gaussian_filter, binary_dilation
 from scipy import stats
 
 
-def nrm_white_level(image, z=0.5, p=80, s=20) -> Image:
+__all__ = ['binarize', 'normalize']
+
+
+def _nrm_white_level(image, z=0.5, p=80, s=20) -> Image:
     """
     Flatten PIL Image by estimating the local white level.
 
@@ -17,9 +21,6 @@ def nrm_white_level(image, z=0.5, p=80, s=20) -> Image:
     :param s: x and y size for multidimensional percentile filter.
     :return:
     """
-    # Forked from DUP-Ocropy.nlbin and changed to work with Python 3
-    # Copyright 2014 Thomas M. Breuel
-    # http://www.apache.org/licenses/LICENSE-2.0
     flat = zoom(image, z)  # zoom in for speed
     flat = percentile_filter(flat, p, size=(s, 2))  # percentile filter x
     flat = percentile_filter(flat, p, size=(2, s))  # percentile filter y
@@ -29,7 +30,7 @@ def nrm_white_level(image, z=0.5, p=80, s=20) -> Image:
     return flat
 
 
-def nrm_thresholds(image, border=0.1, s=1.0, low=5, high=90) -> tuple[float, float]:
+def _nrm_thresholds(image, border=0.1, s=1.0, low=5, high=90) -> tuple[float, float]:
     """
     Calculate low and high thresholds.
 
@@ -40,9 +41,6 @@ def nrm_thresholds(image, border=0.1, s=1.0, low=5, high=90) -> tuple[float, flo
     :param high: percentile for white elimination.
     :return: tuple of low and high thresholds.
     """
-    # Forked from DUP-Ocropy.nlbin and changed to work with Python 3
-    # Copyright 2014 Thomas M. Breuel
-    # http://www.apache.org/licenses/LICENSE-2.0
     d0, d1 = image.shape
     o0, o1 = int(border * d0), int(border * d1)
     est = image[o0:d0-o0, o1:d1-o1]
@@ -58,32 +56,50 @@ def nrm_thresholds(image, border=0.1, s=1.0, low=5, high=90) -> tuple[float, flo
     return lo, hi
 
 
-def image_normalize(im, out_path: Path | None = None) -> Image:
-    """ Normalizes image """
-    # convert image to grayscale and numpy array
-    gs_image = np.array(im.convert('L'))
+def binarize(im: Image, target: Path | None = None, threshold: int = 50) -> Image:
+    """
+    Binarize PIL Image object. Skips processing if image is already binary.
+
+    :param im: Image object.
+    :param target: save binarized image to this path.
+    :param threshold: threshold percentage.
+    :return: binarized Image object.
+    """
+    if not is_bitonal(im):
+        im = nlbin(im, (float(threshold) / 100.0))
+    if target is not None:
+        im.save(target)
+    return im
+
+
+def normalize(im: Image, target: Path | None = None) -> Image:
+    """
+    Normalize PIL Image object.
+
+    :param im: Image object.
+    :param target: save normalized image to this path.
+    :return: normalized Image object.
+    """
+    # All used functions for normalization are forked from:
+    # https://github.com/ocropus-archive/DUP-ocropy/blob/master/ocropus-nlbin
+    # and changed to work with Python 3
+    # Copyright 2014 Thomas M. Breuel (http://www.apache.org/licenses/LICENSE-2.0)
+
+    gs_image = np.array(im.convert('L'))  # convert image to grayscale and numpy array
 
     # normalize the image
     nrm_img = (gs_image - np.min(gs_image)) / (np.max(gs_image) - np.min(gs_image))
 
     # estimate the local white level
-    wl_img = nrm_white_level(nrm_img)
+    wl_img = _nrm_white_level(nrm_img)
 
     # rescale the image to get the gray scale image
-    lo, hi = nrm_thresholds(wl_img)
+    lo, hi = _nrm_thresholds(wl_img)
     wl_img -= lo
     wl_img /= (hi - lo)
     sc_img = np.clip(wl_img, 0, 1)
 
     # convert the image back to PIL image
     im_norm = Image.fromarray((sc_img * 255).astype(np.uint8))
-    if out_path is not None:
-        im_norm.save(out_path)
-
-
-def image_binarize(im: Image, out_path: Path | None = None, threshold: float = 0.5) -> Image:
-    """ Binarizes image """
-    im_bin = nlbin(im, threshold)
-    if out_path is not None:
-        im_bin.save(out_path)
-    return im_bin
+    if target is not None:
+        im_norm.save(target)
