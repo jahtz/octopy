8a17924ba17cfde38afebab02d4d98ed7bf67e79
jahtz <jahtz.dev@proton.me>
2024-09-08 10:49:48 +0200

    clean branch

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .modules.preproc import preprocess, binarize, normalize, resize
-from .modules.segment import segment
-from .modules.segtrain import segtrain
--- __main__.py
+++
@@ -1,7 +0,0 @@
-from octopy import cli
-
-if __name__ == '__main__':
-    """
-    Main entry point for octopy CLI without calling octopy.py directly.
-    """
-    cli()
