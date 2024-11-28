f9fbf6ea7962f42d54e6914f50172bf974ff2383
jahtz <jahtz.dev@proton.me>
2024-11-28 15:09:58 +0100

    update structure to python package and clear existing files

---
+++ src/cli/octopy_cli.py
@@ -0,0 +1,33 @@
+# Copyright 2024 Janik Haitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rich_click as click
+
+__version__ = "5.2.9"
+__prog__ = "octopy"
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.MAX_WIDTH = 96
+
+
+@click.group()
+@click.help_option("--help")
+@click.version_option(__version__,
+                      "--version",
+                      prog_name=__prog__,
+                      message=f"{__prog__} v{__version__} - Developed at Centre for Philology and Digitality (ZPD), "
+                              f"University of Würzburg")
+def cli(**kwargs):
+    """
+    Command line tool layout analysis and OCR of historical prints using Kraken.
+    """
